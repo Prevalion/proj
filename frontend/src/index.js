a613859f0bfb9ede27af0a0e2fb671dc47d473f0
Ayoub Ayoun <medayoubgh0@gmail.com>
2025-04-10 00:23:58 +0100

    fix index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
@@ -12,24 +12,24 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import PrivateRoute from './components/PrivateRoute';
-import AdminRoute from './components/AdminRoute';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
-import PaymentScreen from './screens/PaymentScreen';
-import PlaceOrderScreen from './screens/PlaceOrderScreen';
-import OrderScreen from './screens/OrderScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import OrderListScreen from './screens/admin/OrderListScreen';
-import ProductListScreen from './screens/admin/ProductListScreen';
-import ProductEditScreen from './screens/admin/ProductEditScreen';
-import UserListScreen from './screens/admin/UserListScreen';
-import UserEditScreen from './screens/admin/UserEditScreen';
-import store from './store';
+import PrivateRoute from './components/PrivateRoute.js';
+import AdminRoute from './components/AdminRoute.js';
+import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
+import CartScreen from './screens/CartScreen.js';
+import LoginScreen from './screens/LoginScreen.js';
+import RegisterScreen from './screens/RegisterScreen.js';
+import ShippingScreen from './screens/ShippingScreen.js';
+import PaymentScreen from './screens/PaymentScreen.js';
+import PlaceOrderScreen from './screens/PlaceOrderScreen.js';
+import OrderScreen from './screens/OrderScreen.js';
+import ProfileScreen from './screens/ProfileScreen.js';
+import OrderListScreen from './screens/admin/OrderListScreen.js';
+import ProductListScreen from './screens/admin/ProductListScreen.js';
+import ProductEditScreen from './screens/admin/ProductEditScreen.js';
+import UserListScreen from './screens/admin/UserListScreen.js';
+import UserEditScreen from './screens/admin/UserEditScreen.js';
+import store from './store.js';
 import { Provider } from 'react-redux';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
